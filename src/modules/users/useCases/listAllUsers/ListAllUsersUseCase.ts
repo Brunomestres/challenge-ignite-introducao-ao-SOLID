2496b19d1e390b20a9c55f33168126b5bc59c133
Bruno <brunomestres@gmail.com>
2021-12-10 23:09:58 -0300

    :heavy_check_mark: Desafio concluido

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,19 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const users = this.usersRepository.list();
+
+    const isAdmin = users.find((user) => user.id === user_id);
+
+    if (!isAdmin) {
+      throw new Error("User not found");
+    }
+
+    if (!isAdmin.admin) {
+      throw new Error("User not admin");
+    }
+
+    return users;
   }
 }
 
